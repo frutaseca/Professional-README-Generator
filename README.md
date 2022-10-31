aec0f65fcbc0bb4f22ef14d9c786df4f53ed6cd8
Brandon <brandonrodriguezvega@gmail.com>
2022-10-31 17:22:34 -0400

    Project done and finshed just need to link walkthrough video

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
-# Professional-README-Generator
+# README Generator
 
-## What the App is for
+## Desciption
+This Project was created using nodejs and the packages needed to get this to work. This is the first project using backend so it was a little challenging not as much as the other front end one towards the end of the front end section of the class. This README Generator could also be used for future projects to help write a README quicker.
 
-## How to install app
+## Table of Contents
 
-## How to report issues
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Test](#test)
+* [Github](#github)
+* [Email](#email)
 
-## How to contribute
+## Installation
+First step was making the repo and cloning it to our local device. Then we make our .gitignore folder adding node_modules/ and .DS_Store/ in there and making our index.js file. We put npm init -y and npm i inquirer into our comand line to get the packages needed. Then the code is writen inside the index.js file.
+
+## Usage
+The main way to use this is by going into the command line in your terminal and typing node index.js and answer the answers to get the desired result of a generated README file.
+
+## License
+![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Contributing
+You may contribute if you may like the only contributors are the owner of this repository.
+
+## Tests
+The tests that can be done are making README files.
+
+## Questions?
+If any questions contact at<br/>
+Github: https://github.com/frutaseca<br/>
+Email: brandonrodriguezvega@gmail.com
+
+## Link to walkthrough video
+
--- index.js
+++ index.js
@@ -1,2 +1,100 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const generateREADME = ({title, description, install, usage, contributing, tests, license, github, email}) =>
+`# ${title}
+
+## Desciption
+${description}
+
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Test](#test)
+* [Github](#github)
+* [Email](#email)
+
+## Installation
+${install}
+
+## Usage
+${usage}
+
+## License
+![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)
+
+## Contributing
+${contributing}
+
+## Tests
+${tests}
+
+## Questions?
+If any questions contact at<br/>
+Github: https://github.com/${github}<br/>
+Email: ${email}
+
+## Link to walkthrough video
+
+`
+
+inquirer
+.prompt([
+    {
+        type:'input',
+        name:'title',
+        message:'What is the title of your README?',
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'Write a description.',
+    },
+    {
+        type:'input',
+        name:'install',
+        message:'Write about installation instructions.',
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:'Write about usage information.',
+    },
+    {
+        type:'input',
+        name:'contributing',
+        message:'Write about contribution guidelines.',
+    },
+    {
+        type:'input',
+        name:'tests',
+        message:'Write about test instructions.',
+    },
+    {
+        type:'list',
+        name:'license',
+        message:'What License shall you chocie?',
+        choices: ['ISC', 'PostgreSPL', 'MIT', 'Apache license 2.0', 'Artistic license 2.0', 'Microsoft Public License'],
+    },
+    {
+        type:'input',
+        name:'github',
+        message:'What is you Github username?',
+    },
+    {
+        type:'input',
+        name:'email',
+        message:'What is your email?',
+    },
+])
+.then((answers) => {
+    const READMEPageContent = generateREADME(answers);
+
+    fs.writeFile('README.md', READMEPageContent, (err) =>
+    err ? console.log(err) : console.log('Successfully created README file!')
+    );
+})
